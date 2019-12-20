670cec94e4458b3a7f7737f52e989ca529061152
Carlos Hui <huizhen_2014@163.com>
2019-12-20 15:38:49 +0800

    Add files via upload

--- ggplot2_for_volcano.r
+++ ggplot2_for_volcano.r
@@ -1,32 +1,35 @@
+##Plot the vocalno pic with Different expression genes matrix
 library(ggplot2)
 library(ggrepel)
-##get the table of edgeR $count
-table_43vsLAC_4 <- lrt_0.04_43vsLAC_4$table
-##distribute the color according to the logFC and PValue from table_43vsLAC_4
-New_table_43vsLAC_4 <- within(table_43vsLAC_4,{
-  color <- NA
-  color[logFC < -1 & PValue < 0.01 ] <- "green"
-  color[logFC > 1 & PValue < 0.01 ] <- "red"
-  color[logFC > -1 & logFC < 1 ] <- "black"
-  color[PValue > 0.01 ] <- "grey"
-})
+library(xlsx)
+
+##Get the Expression table
+MS_Exp <- read.xlsx("MS_identified_information.xlsx",2,header=T,
+                    colClasses = "character")
+##Modify MS_Exp by adding Product iterms
+MS_Exp$Product <- substr(MS_Exp$Protein.description,1,
+                         regexpr("OS=",MS_Exp$Protein.description)-1)
+
+##Assign the color
+MS_Exp_1.2 <- within(MS_Exp,{
+  Expression <- NA ##必须指定, Not Available
+  Expression[(ampR_1.ampR.Ratio <= 1.2)|(ampR_1.ampR.Ratio >= 1/1.2)] <- "NA"
+  Expression[(ampR_1.ampR.Ratio > 1.2) & (ampR_1.ampR.P.value < 0.05)] <- "High"
+  Expression[(ampR_1.ampR.Ratio < 1/1.2) & (ampR_1.ampR.P.value < 0.05)] <- "Low"
+}
+)
+
 ##添加名称列，可对应显示名称信息,对应将需要显示的名称添加到新列Name，其他为NA即可
 ##geom_text(aes(label=Name), size=4)
-colours <- c(red="red",green="green",black="black",grey="grey")
-ggplot(New_table_43vsLAC_4,aes(logFC,-log10(PValue)))+geom_point(aes(color=color))+
-  scale_color_manual(values=colours)+labs(title="43 vs LAC_4",x="logFC",y="-log10(PValue")+
-  geom_hline(yintercept = range(-log10(0.01),-log10(0.001)),color=c("blue","red"),linetype="dotted")+
-  geom_vline(xintercept=range(-1,1),color="yellow",linetype="solid")
-##save the pic
-ggsave("43_vs_LAC_4_valcono.png",with=5,height=5)
-
-##更新版，同时修改图例显示内容
-#lrt_bcv_0.2_table_new_new <- within(lrt_bcv_0.2_table_new,{
-# color_new <- NA
-# color_new[logFC > 1 & PValue < 0.05 ] <- "logFC>1&PValue<0.05"
-# color_new[logFC < -1 & PValue < 0.05 ] <- "logFC<-1&PValue<0.05"
-# color_new[logFC > -1 & logFC < 1 ] <- "-1<logFC<1"
-# color_new[PValue > 0.05 ]<- "PValue>0.05"
-# })
-#colours <- c("-1<logFC<1"="black","logFC<-1&PValue<0.05"="green","PValue>0.05"="grey","logFC>1&PValue<0.05"="red")
-#ggplot(lrt_bcv_0.2_table_new_new,aes(logFC,-log10(PValue)))+geom_point(aes(color=color_new))+scale_color_manual(values=colours)
+colours <- c("High"="red","Low"="green","NA"="grey")
+ggplot(MS_Exp_1.2,aes(log(ampR_1.ampR.Ratio,2),-log(ampR_1.ampR.P.value,10)))+
+  geom_point(aes(color=Expression))+
+  scale_color_manual(values=colours,labels=c("High","Low","Unsig"))+
+  theme_bw()+ ylim(0,3.5)+xlim(-6,6)+
+  geom_label_repel(aes(log(ampR_1.ampR.Ratio,2),-log(ampR_1.ampR.P.value,10),
+                       label=ifelse(Expression != "NA",Product,"")),size=3)+
+  labs(x=expression(log[2](Fold)),y=expression(-log[10](Pvalue)))+
+  geom_hline(yintercept =-log10(0.05),color="black",linetype="dotted")+
+  geom_vline(xintercept = range(log(1/1.2,2),log(1.2,2)),color=c("green","red"),linetype="dotted")
+##Save the pic
+ggsave("MS_Exp_1.2.pdf",width=6,height = 7.5)
